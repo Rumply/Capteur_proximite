971c7e46db8a5fe51d6df505a7c2218e5e175b05
Rumply <rumplygnat@hotmail.com>
2016-09-26 15:26:50 -0400

    Second commit

---
+++ app/src/main/java/ca/cegepdrummond/test/MainActivity.java
@@ -0,0 +1,64 @@
+package ca.cegepdrummond.test;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
+
+public class MainActivity extends Activity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+    private Sensor mProximity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if (event.values[0] == 0) {
+                //Proche
+                getWindow().getDecorView().setBackgroundColor(Color.argb(125,0,0,0));
+            } else {
+                //Loin
+                getWindow().getDecorView().setBackgroundColor(Color.argb(255,255,255,255));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onBackPressed(){
+
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+}
